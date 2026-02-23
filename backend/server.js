815c39cf2aeedfb05ab1daa117a1510534b1c6cf
Ritesh Davande <ritsdavande@gmail.com>
2026-02-23 12:53:29 +0530

    fix: add vercel.json configs to fix 404 routing errors on Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
 app.use(express.json());
 
 // API endpoints
